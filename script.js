45ff6cae52866584eb7f2f521cea5c02d541008d
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-03-28 10:53:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,31 +4,43 @@
  * FLOWCHART: https://lucid.app/lucidchart/5a3164fd-459f-494d-9cae-b4a6be593b13/view
  */
 
-
-/* main controls the program. Calling askQuestions() provides feedback depending on the 
+main()
+/* main controls the program. Calling askQuetions() it provides feedback depending on the 
  * number right returned: Either "Perfect!" or it says how many right out of the number asked. 
- * Store the number of questions to ask in a variable called questions.
+ * Set the numbers of quetions as the variable called quetions.
  * @param none
  * @return none
  */
 function main() {
-
+    let questions = 5;
+    let right = askQuestions(questions);
+    if (right == questions) alert("Perfect!");
+    else alert("You got " + right + " out of " + questions);
 }
 
-/* askQuestions calls askQuestion() question number if times, sending the question number as an argument. 
- * It counts the number right returned, and return number right to main().
+/* askQuetions calls askQuestion() question times, sending the question number as an argument. 
+ * It counts number wrong returned, and return number wrong to main().
  * @param: none
- * @return: {integer} score (0-questions)
+ * @return: score (0-Quetions)
  */
-function askQuestions() {
-
+function askQuestions(quetions) {
+    let question = 1;
+    while (question <= questions) {
+        right += askQuestions(question);
+        question ++;
+    }
+    return right;
 }
 
-/* askQuestion asks a multiplication question, using the question parameter to say which
+/* askQuestion asks a multiplication question, using the quesiton parameter to say which
  * question is being asked.  It returns 1 if correct, 0 if incorrect.
- * @param: {integer} question 
- * @return: {integer} (0 or 1) or {boolean}
+ * @param: question (integer 1-5)
+ * @return: integer (0 or 1)
  */
 function askQuestion(question){
+  askQuestion(question);
+  let a = Math.floor(Math.random()*9)+1;
+  let b = Math.floor(Math.random()*9)+1;
+  let product = a * b;
   
 }
